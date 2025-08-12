757df2281ff228af0cb65fca1400ace0ee922ed5
lokeshsai-l7 <lokeshsai.polishetty@gmail.com>
2025-08-12 22:12:06 +0530

    Added mobile menu navigation

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -10,10 +10,15 @@ import {
 } from "../components/lightswind/navigation-menu";
 import avatarImg from "../assets/profile-pictures/avatar.jpeg";
 import { HiOutlineMenu } from "react-icons/hi";
+import { ImCross } from "react-icons/im";
+import { RxCross2 } from "react-icons/rx";
 import { useState } from "react";
 
 export default function Header() {
   const [active, setActive] = useState("home");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
 
   const navLinks = [
     { href: "#home", label: "Home", id: "home" },
@@ -25,7 +30,7 @@ export default function Header() {
   ];
 
   return (
-    <header className="mt-4 flex sticky justify-center w-full max-w-2xl">
+    <header className="mt-4  sticky justify-center w-full max-w-2xl">
       <div className="bg-white rounded-xl p-2  w-full flex justify-between items-center">
         <Avatar className="mr-5">
           <AvatarImage src={avatarImg} alt="avatar" />
@@ -48,10 +53,30 @@ export default function Header() {
             ))}
           </NavigationMenuItem>
         </NavigationMenu>
-        <div className="md:hidden text-3xl">
-          <HiOutlineMenu />
-        </div>
+
+        <button
+          onClick={toggleMenu}
+          className="md:hidden text-3xl focus:outline-none"
+        >
+          {isOpen ? <RxCross2 /> : <HiOutlineMenu />}
+        </button>
       </div>
+      {isOpen && (
+        <ul className="md:hidden mt-3 space-y-2 bg-white  p-4 rounded-lg ">
+          {navLinks.map((link) => (
+            <li onClick={() => setActive(link.id)} key={link.id}>
+              <a
+                href={link.href}
+                className={`block transition duration-200 transform hover:scale-105 ${
+                  active === link.id ? "underline" : ""
+                }`}
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </header>
   );
 }
